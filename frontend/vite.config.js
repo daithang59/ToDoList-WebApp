fdde9c2fd248875161986b866ca0dd63e98023cf
Dai Thang <23521422@gm.uit.edu.vn>
2025-11-16 15:37:41 +0700

    Allow OKD Route domain for Vite preview

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,7 +2,13 @@ import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  preview: {
+    host: true,
+    port: 8080,
+    allowedHosts: [
+      "todolist-frontend-route-todo-webapp.apps.okd.hldthang.io.vn",
+    ],
+  },
 });
